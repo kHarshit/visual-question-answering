6f7c8f1d86595fe80d7f75b075abf0061aa6f2f0
Harshit Kumar <kumar.hars@northeastern.edu>
2024-08-15 22:32:15 -0400

    update app.py

--- app.py
+++ app.py
@@ -22,8 +22,75 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
 # Load the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("MODEL_NAME")
-model = AutoModel.from_pretrained("MODEL_NAME")
+
+class MultimodalVQAModel(nn.Module):
+    def __init__(
+            self,
+            num_labels: int = len(answer_space),
+            intermediate_dim: int = 512,
+            pretrained_text_name: str = 'bert-base-uncased',
+            pretrained_image_name: str = 'google/vit-base-patch16-224-in21k'):
+     
+        super(MultimodalVQAModel, self).__init__()
+        self.num_labels = num_labels
+        self.pretrained_text_name = pretrained_text_name
+        self.pretrained_image_name = pretrained_image_name
+        
+        self.text_encoder = AutoModel.from_pretrained(
+            self.pretrained_text_name,
+        )
+        self.image_encoder = AutoModel.from_pretrained(
+            self.pretrained_image_name,
+        )
+        self.fusion = nn.Sequential(
+            nn.Linear(self.text_encoder.config.hidden_size + self.image_encoder.config.hidden_size, intermediate_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+        )
+        
+        self.classifier = nn.Linear(intermediate_dim, self.num_labels)
+        
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def forward(
+            self,
+            input_ids: torch.LongTensor,
+            pixel_values: torch.FloatTensor,
+            attention_mask: Optional[torch.LongTensor] = None,
+            token_type_ids: Optional[torch.LongTensor] = None,
+            labels: Optional[torch.LongTensor] = None):
+        
+        encoded_text = self.text_encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            return_dict=True,
+        )
+        encoded_image = self.image_encoder(
+            pixel_values=pixel_values,
+            return_dict=True,
+        )
+        fused_output = self.fusion(
+            torch.cat(
+                [
+                    encoded_text['pooler_output'],
+                    encoded_image['pooler_output'],
+                ],
+                dim=1
+            )
+        )
+        logits = self.classifier(fused_output)
+        
+        out = {
+            "logits": logits
+        }
+        if labels is not None:
+            loss = self.criterion(logits, labels)
+            out["loss"] = loss
+        
+        return out
+
+model = MultimodalVQAModel()
 model.to(device)
 model.eval()
 
